53a2f0fa26ee66ff122bb05ca36ea06019bfc04a
asalex04 <arc04@mail.ru>
2020-11-03 16:34:12 +0400

    create program.js

---
+++ src/program.js
@@ -0,0 +1,15 @@
+import commander from 'commander';
+import genDiff from '.';
+
+export default program = () => {
+  commander
+    .version('1.0.0')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format [pretty]', 'pretty')
+    .arguments('<firstConfig> <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      console.log(genDiff(firstConfig, secondConfig, commander.format));
+    });
+
+  commander.parse(process.argv);
+};
