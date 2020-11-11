b6ea96ba028bfb3477aef92187655034aad224c0
asalex04 <arc04@mail.ru>
2020-11-11 15:55:02 +0400

    add index.js

--- src/program.js
+++ src/program.js
@@ -4,7 +4,8 @@ const program = () => {
   commander
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format <type>', 'output format [pretty]', 'pretty');
+    .option('-f, --format <type>', 'output format [pretty]', 'pretty')
+    .arguments('<filepath1> <filepath2>');
 
   commander.parse(process.argv);
 };
