60997afae1502330ce09609ef7ee41d3553aaed4
asalex04 <arc04@mail.ru>
2020-11-23 17:12:40 +0400

    modified:   ../package.json 	modified:   ../src/index.js 	modified:   ../src/program.js

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const dataFile = (filepath) => {
+  const pathToFile = path.resolve(process.cwd(), filepath);
+  const data = fs.readFileSync(pathToFile, 'utf-8');
+  const type = path.extname(filepath).slice(1);
+  return JSON.parse(data, type);
+};
+
+const bildAST = (dataBefore, dataAfter) => {
+  const keys = _.union(Object.keys(dataBefore), Object.keys(dataAfter));
+  const sortKeys = _.sortBy(keys);
+  const AST = sortKeys.map((key) => {
+    const node = {
+      name: key,
+      oldValue: dataBefore[key],
+      newValue: dataAfter[key],
+    };
+    if (!_.has(dataAfter, key)) return { ...node, status: 'deleted' };
+    if (!_.has(dataBefore, key)) return { ...node, status: 'added' };
+    if (dataBefore[key] !== dataAfter[key]) return { ...node, status: 'edited' };
+    return { ...node, status: 'unchanged' };
+  });
+  return AST;
+};
+
+const format = (obj) => {
+  const tab = '  ';
+  const res = obj.map((node) => {
+    const {
+      name, oldValue, newValue, status,
+    } = node;
+    const lines = {
+      added: () => `${tab}+ ${name}: ${newValue}`,
+      deleted: () => `${tab}- ${name}: ${oldValue}`,
+      unchanged: () => `${tab}${tab}${name}: ${oldValue}`,
+      edited: () => [lines.added(), lines.deleted()],
+    };
+    return lines[status]();
+  });
+  const result = _.flatten(res).join('\n');
+  return `{\n${result}\n}`;
+};
+
+const genDiff = (firstFile, secondFile) => {
+  const dataBefore = dataFile(firstFile);
+  const dataAfter = dataFile(secondFile);
+  const Tree = bildAST(dataBefore, dataAfter);
+  return format(Tree);
+};
+
+export default genDiff;
--- src/program.js
+++ src/program.js
@@ -1,11 +1,15 @@
 import commander from 'commander';
+import genDiff from './index.js';
 
 const program = () => {
   commander
     .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format [pretty]', 'pretty')
-    .arguments('<filepath1> <filepath2>');
+    .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => {
+      console.log(genDiff(filepath1, filepath2));
+    });
 
   commander.parse(process.argv);
 };
