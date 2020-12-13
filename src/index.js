37dea05d6d6853705063e61bbe087f10eb870020
asalex04 <arc04@mail.ru>
2020-12-13 12:08:46 +0400

    modified:   after.json 	modified:   before.json 	modified:   fileResult.txt 	modified:   ../../src/index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
 import parse from './parsers.js';
+import diff from './formaters/stylish.js';
 
 const dataFile = (filepath) => {
   const pathToFile = path.resolve(process.cwd(), filepath);
   const data = fs.readFileSync(pathToFile, 'utf-8');
   const type = path.extname(filepath).slice(1);
-  // console.log(data);
-  // console.log(type);
-  // console.log(parse(data, type));
   return parse(data, type);
 };
 
@@ -22,6 +20,13 @@ const bildAST = (dataBefore, dataAfter) => {
       oldValue: dataBefore[key],
       newValue: dataAfter[key],
     };
+    if (_.isObject(dataBefore[key]) && _.isObject(dataAfter[key])) {
+      return {
+        name: key,
+        status: 'hasChildren',
+        currentValue: bildAST(dataBefore[key], dataAfter[key]),
+      };
+    }
     if (!_.has(dataAfter, key)) return { ...node, status: 'deleted' };
     if (!_.has(dataBefore, key)) return { ...node, status: 'added' };
     if (dataBefore[key] !== dataAfter[key]) return { ...node, status: 'edited' };
@@ -30,29 +35,11 @@ const bildAST = (dataBefore, dataAfter) => {
   return AST;
 };
 
-const format = (obj) => {
-  const tab = '  ';
-  const res = obj.map((node) => {
-    const {
-      name, oldValue, newValue, status,
-    } = node;
-    const lines = {
-      added: () => `${tab}+ ${name}: ${newValue}`,
-      deleted: () => `${tab}- ${name}: ${oldValue}`,
-      unchanged: () => `${tab}${tab}${name}: ${oldValue}`,
-      edited: () => [lines.added(), lines.deleted()],
-    };
-    return lines[status]();
-  });
-  const result = _.flatten(res).join('\n');
-  return `{\n${result}\n}`;
-};
-
 const genDiff = (firstFile, secondFile) => {
   const dataBefore = dataFile(firstFile);
   const dataAfter = dataFile(secondFile);
   const Tree = bildAST(dataBefore, dataAfter);
-  return format(Tree);
+  return diff(Tree);
 };
 
 export default genDiff;
