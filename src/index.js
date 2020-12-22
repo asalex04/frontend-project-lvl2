ef40ee36752caecc6d0978518115c82c8d011e20
asalex04 <arc04@mail.ru>
2020-12-22 16:50:48 +0400

    modified:   ../../src/index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import _ from 'lodash';
 import parse from './parsers.js';
 import diff from './formaters/index.js';
 
-const dataFile = (filepath) => {
-  const pathToFile = path.resolve(process.cwd(), filepath);
-  const data = fs.readFileSync(pathToFile, 'utf-8');
-  const type = path.extname(filepath).slice(1);
-  return parse(data, type);
+const getDataFile = (filepath) => {
+  const fullPath = path.resolve(process.cwd(), filepath);
+  const data = fs.readFileSync(fullPath, 'utf-8');
+  const fileExtension = path.extname(filepath).slice(1);
+  return parse(data, fileExtension);
 };
 
-const bildAST = (dataBefore, dataAfter) => {
+const bildTree = (dataBefore, dataAfter) => {
   const keys = _.union(Object.keys(dataBefore), Object.keys(dataAfter));
-  const sortKeys = _.sortBy(keys);
-  const AST = sortKeys.map((key) => {
+  const sortedKeys = _.sortBy(keys);
+  const diffTree = sortedKeys.map((key) => {
     const node = {
       name: key,
       oldValue: dataBefore[key],
@@ -24,7 +24,7 @@ const bildAST = (dataBefore, dataAfter) => {
       return {
         name: key,
         status: 'hasChildren',
-        currentValue: bildAST(dataBefore[key], dataAfter[key]),
+        currentValue: bildTree(dataBefore[key], dataAfter[key]),
       };
     }
     if (!_.has(dataAfter, key)) return { ...node, status: 'removed' };
@@ -32,13 +32,13 @@ const bildAST = (dataBefore, dataAfter) => {
     if (dataBefore[key] !== dataAfter[key]) return { ...node, status: 'updated' };
     return { ...node, status: 'unchanged' };
   });
-  return AST;
+  return diffTree;
 };
 
 const genDiff = (firstFile, secondFile, format = 'stylish') => {
-  const dataBefore = dataFile(firstFile);
-  const dataAfter = dataFile(secondFile);
-  const Tree = bildAST(dataBefore, dataAfter);
+  const dataBefore = getDataFile(firstFile);
+  const dataAfter = getDataFile(secondFile);
+  const Tree = bildTree(dataBefore, dataAfter);
   return diff(Tree, format);
 };
 
