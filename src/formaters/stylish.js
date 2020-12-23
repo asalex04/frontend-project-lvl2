d10d7cfbba66d9b47076ee1189359a2d8aaf2572
asalex04 <arc04@mail.ru>
2020-12-23 14:09:44 +0400

    modified:   src/formaters/plain.js 	modified:   src/formaters/stylish.js

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const tab = (n) => ' '.repeat(n);
+const getTab = (n) => ' '.repeat(n);
 const gap = '  ';
 
 const modify = (element, depth) => {
@@ -8,8 +8,8 @@ const modify = (element, depth) => {
     return element;
   }
   const pair = Object.entries(element);
-  const res = pair.map(([key, value]) => `${tab(depth + 5)}${key}: ${value}`);
-  return ['{', ...res, `${tab(depth + 2)}}`].join('\n');
+  const res = pair.map(([key, value]) => `${getTab(depth + 5)}${key}: ${value}`);
+  return ['{', ...res, `${getTab(depth + 2)}}`].join('\n');
 };
 
 const iter = (tree, depth) => {
@@ -18,15 +18,15 @@ const iter = (tree, depth) => {
       name, oldValue, newValue, status, currentChildren,
     } = node;
     const lines = {
-      added: () => `${gap}${tab(depth)}+ ${name}: ${modify(newValue, depth)}`,
-      removed: () => `${gap}${tab(depth)}- ${name}: ${modify(oldValue, depth)}`,
-      unchanged: () => `${gap}${tab(depth + 2)}${name}: ${modify(oldValue, depth)}`,
+      added: () => `${gap}${getTab(depth)}+ ${name}: ${modify(newValue, depth)}`,
+      removed: () => `${gap}${getTab(depth)}- ${name}: ${modify(oldValue, depth)}`,
+      unchanged: () => `${gap}${getTab(depth + 2)}${name}: ${modify(oldValue, depth)}`,
       changed: () => `${lines.added()}\n${lines.removed()}`,
-      hasChildren: () => `${gap}${tab(depth + 2)}${name}: ${iter(currentChildren, depth + 2)}`,
+      hasChildren: () => `${gap}${getTab(depth + 2)}${name}: ${iter(currentChildren, depth + 2)}`,
     };
     return lines[status]();
   });
-  return ['{', ...res, `${tab(depth)}}`].join('\n');
+  return ['{', ...res, `${getTab(depth)}}`].join('\n');
 };
 
 export default (tree) => iter(tree, 0);
