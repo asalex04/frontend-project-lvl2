4a9bd3f7a9e243a83bf81bb8ce7dd65e335ba0a3
asalex04 <arc04@mail.ru>
2020-12-28 17:13:52 +0400

    new file:   src/buildTree.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import _ from 'lodash';
 import parse from './parsers.js';
+import buildTree from './buildTree.js';
 import diff from './formaters/index.js';
 
 const getFileData = (filepath) => {
@@ -9,47 +9,6 @@ const getFileData = (filepath) => {
   const data = fs.readFileSync(fullPath, 'utf-8');
   const dataFormat = path.extname(filepath).slice(1);
   return parse(data, dataFormat);
-};
-
-const buildTree = (dataBefore, dataAfter) => {
-  const keys = _.union(Object.keys(dataBefore), Object.keys(dataAfter));
-  const diffTree = _.sortBy(keys).map((key) => {
-    if (!_.has(dataAfter, key)) {
-      return {
-        name: key,
-        status: 'removed',
-        oldValue: dataBefore[key],
-      };
-    }
-    if (!_.has(dataBefore, key)) {
-      return {
-        name: key,
-        status: 'added',
-        newValue: dataAfter[key],
-      };
-    }
-    if (_.isPlainObject(dataBefore[key]) && _.isPlainObject(dataAfter[key])) {
-      return {
-        name: key,
-        status: 'hasChildren',
-        currentChildren: buildTree(dataBefore[key], dataAfter[key]),
-      };
-    }
-    if (dataBefore[key] !== dataAfter[key]) {
-      return {
-        name: key,
-        status: 'changed',
-        newValue: dataAfter[key],
-        oldValue: dataBefore[key],
-      };
-    }
-    return {
-      name: key,
-      status: 'unchanged',
-      oldValue: dataBefore[key],
-    };
-  });
-  return diffTree;
 };
 
 const genDiff = (filePathBefore, filePathAfter, format = 'stylish') => {
