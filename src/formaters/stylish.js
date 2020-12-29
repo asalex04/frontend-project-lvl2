de3b28a7c13888ea44defbfeb528a6182f3fd3bb
asalex04 <arc04@mail.ru>
2020-12-29 10:40:14 +0400

    deleted:    __fixtures__/result.txt 	modified:   __fixtures__/resultStylish.txt 	modified:   src/bin/gendiff.js 	modified:   src/formaters/stylish.js

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -6,20 +6,22 @@ const modify = (element, depth) => {
   if (!_.isObject(element)) {
     return element;
   }
-  const entries = Object.entries(element);
-  const result = entries.map(([key, value]) => `${getTab(depth + 2)}${key}: ${value}`);
-  return ['{', ...result, `${getTab(depth)}}`].join('\n');
+  const result = _.entries(element)
+    .map(([key, value]) => `${getTab(depth + 2)}${key}: ${modify(value, depth + 2)}`);
+  const output = ['{', ...result, `${getTab(depth)}}`].join('\n');
+  // return _.isObject(output) ? modify() : output;
+  return output;
 };
 
 const iter = (tree, depth) => {
   const output = tree.map((node) => {
     const { name, status } = node;
     const mapping = {
+      hasChildren: () => `${getTab(depth + 1)}${name}: ${iter(node.currentChildren, depth + 2)}`,
       added: () => `${getTab(depth)}+ ${name}: ${modify(node.newValue, depth + 1)}`,
       removed: () => `${getTab(depth)}- ${name}: ${modify(node.oldValue, depth + 1)}`,
       unchanged: () => `${getTab(depth + 1)}${name}: ${modify(node.oldValue, depth + 1)}`,
       changed: () => `${mapping.removed()}\n${mapping.added()}`,
-      hasChildren: () => `${getTab(depth + 1)}${name}: ${iter(node.currentChildren, depth + 2)}`,
     };
     return mapping[status]();
   });
