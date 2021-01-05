cad801b1dacbd04b3e13ddc2ebdd79e9943374e3
asalex04 <arc04@mail.ru>
2021-01-05 11:23:48 +0400

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
 import buildTree from './buildTree.js';
-import formater from './formaters/index.js';
+import format from './formaters/index.js';
 
 const getFileData = (filepath) => {
   const fullPath = path.resolve(process.cwd(), filepath);
@@ -11,11 +11,11 @@ const getFileData = (filepath) => {
   return parse(data, dataFormat);
 };
 
-const genDiff = (filePathBefore, filePathAfter, format = 'stylish') => {
+const genDiff = (filePathBefore, filePathAfter, formatName = 'stylish') => {
   const dataBefore = getFileData(filePathBefore);
   const dataAfter = getFileData(filePathAfter);
   const tree = buildTree(dataBefore, dataAfter);
-  return formater(tree, format);
+  return format(tree, formatName);
 };
 
 export default genDiff;
